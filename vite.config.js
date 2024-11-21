8854d02cb698a46129003beaec1d2832abbb4efb
Alexis Garcia <alexisgarciagarcia@alumno.ieselrincon.es>
2024-11-21 18:11:33 +0000

    pakage,jon y veteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+/*
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   //base: '/avisos-push/', // Base del proyecto en el servidor
-})
+})*/
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // Carpeta de salida para la build
+  },
+  server: {
+    port: 3000, // Puerto para desarrollo
+  },
+  preview: {
+    port: 3000, // Puerto para preview en producción
+  },
+});
