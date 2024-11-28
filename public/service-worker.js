d147ac67cc8c9a18fa76815063250693c7e752d9
Alexis Garcia <alexisgarciagarcia@alumno.ieselrincon.es>
2024-11-28 15:00:22 +0000

    link 2

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,32 @@
+// self.addEventListener('push', (event) => {
+//     const data = event.data.json();
+//     console.log('Notificación recibida:', data);
+
+//     self.registration.showNotification(data.title, {
+//         body: data.body,
+//     });
+// });
+
+
 self.addEventListener('push', (event) => {
     const data = event.data.json();
     console.log('Notificación recibida:', data);
 
+    // Mostrar la notificación con el payload recibido
     self.registration.showNotification(data.title, {
         body: data.body,
+        data: { link: data.data?.link }, // Incluir el enlace en el campo "data"
     });
 });
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close(); // Cerrar la notificación
+
+    const link = event.notification.data?.link; // Obtener el enlace desde "data"
+    if (link) {
+        event.waitUntil(clients.openWindow(link)); // Abrir el enlace en una nueva ventana/pestaña
+    }
+});
+
+
+  
